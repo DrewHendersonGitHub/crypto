555cf6b3c4005552962406a70339d12cc4248934
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-18 14:12:44 -0800

    added conversion from crypto to USD
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>

--- src/index.js
+++ src/index.js
@@ -11,23 +11,39 @@ function displayCrypto(response) {
 
 function buildCryptoString(response) {
   let htmlToDisplay = [];
+  const intervalDisplays = ["1d", "30d", "365d"];
   for (let i = 0; i < response.length; i++) {
     htmlToDisplay.push(`<p>Coin Name: ${response[i].name}</p>`);
     htmlToDisplay.push(`<img src= '${response[i].logo_url}' class='coin-logo'>`);
     htmlToDisplay.push(`<p>Price: $${response[i].price}</p>`);
     let marketCapPercent = (response[i].market_cap_dominance * 100).toFixed(2);
     htmlToDisplay.push(`<p>Market Cap Dominance: ${marketCapPercent}%</p>`);
-    htmlToDisplay.push(`<p>1 Day Price Change: `);
-    if (response[i]["1d"].price_change < 0) {
-      htmlToDisplay.push(`-`);
-    } else {
-      htmlToDisplay.push(`+`);
+    for (let j=0; j < intervalDisplays.length; j++) {
+      htmlToDisplay.push(`<p>Price change over ${intervalDisplays[j]}: `);
+      if (response[i][intervalDisplays[j]].price_change < 0) {
+        htmlToDisplay.push(`-`);
+      } else {
+        htmlToDisplay.push(`+`);
+      }
+      htmlToDisplay.push(`$${Math.abs(response[i][intervalDisplays[j]].price_change)}</p>`);
     }
-    htmlToDisplay.push(`$${Math.abs(response[i]["1d"].price_change)}</p>`);
-    htmlToDisplay.push('<hr>');
-    htmlToDisplay.push('<hr>');
+    htmlToDisplay.push(`<hr>`);
   }
   return htmlToDisplay.join('');
+}
+
+function displayConvert(response, coin, amount) {
+  let output = buildConvertString(response, coin, amount);
+  $('#show-info').html(output);
+}
+
+function buildConvertString(response, coin, amount) {
+  for (let i = 0; i < response.length; i++) {
+    if (coin === response[i].currency) {
+      return `<p>${amount} ${coin} is equal to $${amount * response[i].rate} USD.</p>`;      
+    }
+  }
+  return `<p>We couldn't find a conversion rate for ${coin}!`;
 }
 
 function displayErrors(error) {
@@ -42,6 +58,18 @@ $(document).ready(function() {
           throw Error(`Nomic API error: ${cryptoResponse.message}`);
         }
         displayCrypto(cryptoResponse);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
+  });
+  $('#convert').click(function() {
+    CryptoService.convertCrypto()
+      .then(function(cryptoResponse) {
+        if (cryptoResponse instanceof Error) {
+          throw Error(`Nomic API error: ${cryptoResponse.message}`);
+        }
+        displayConvert(cryptoResponse, $('#coin').val(), $('#amount').val());
       })
       .catch(function(error) {
         displayErrors(error.message);

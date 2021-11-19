f7ef367fd1f77c2cc52c6f12a409c8910d40e1cd
Drew Henderson <drewhendersonemail@gmail.com>
2021-11-18 16:35:13 -0800

    env file 4

--- src/crypto.js
+++ src/crypto.js
@@ -1,6 +1,6 @@
 export default class CryptoService {
   static getCrypto() {
-    return fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&interval=1d,30d,365d&status=active&per-page=10&sort=rank`)
+    return fetch(`https://api.nomics.com/v1/currencies/ticker?key=e7d306d239a34ca23a33e50fd074358cd28b800f&interval=1d,30d,365d&status=active&per-page=10&sort=rank`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
@@ -13,7 +13,7 @@ export default class CryptoService {
   }
 
   static convertCrypto() {
-    return fetch(`https://api.nomics.com/v1/exchange-rates?key=${process.env.API_KEY}`)
+    return fetch(`https://api.nomics.com/v1/exchange-rates?key=e7d306d239a34ca23a33e50fd074358cd28b800f`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
